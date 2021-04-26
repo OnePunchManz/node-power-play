1f5c8a7698afdc8ea57f57ae1986e3b39095c811
Zaid Pasha <zaidzpasha@gmail.com>
2021-04-26 16:52:42 -0400

    app fix

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3001 ;
 
 // app.get('/', (req,res) => {
 // 	res.send("Hello Zaid")
